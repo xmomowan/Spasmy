4f6ceeba9b750f8ae021d076bc7bb3aad39c1c12
Ryan Liang <xmomowan@gmail.com>
2018-06-07 23:56:10 +0800

    Actually commit Player

--- Twift/Player/Player.swift
+++ Twift/Player/Player.swift
@@ -8,11 +8,41 @@
 
 import Cocoa
 
-class Player: NSViewController {
+/// The controller for video related stuff
+/// Holds a single reference to a private mpv handle, which means you have to create
+/// another one if you are watching multiple videos
+class Player {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
+    var mpv_handle: OpaquePointer?
+    var mpv_gl: OpaquePointer?
+
+    init() {
+
+        self.mpv_handle = mpv_create()
+        mpv_initialize(self.mpv_handle)
+        self.mpv_gl = OpaquePointer(mpv_get_sub_api(mpv_handle, MPV_SUB_API_OPENGL_CB))
     }
-    
+
+    func makePlayerView(asSubviewOf parent: NSView) -> NSView {
+
+        guard let mpv_gl = mpv_gl else {
+
+            // @todo: better error handling
+            fatalError("mpv OpenGL failed")
+        }
+
+        let view = MPVOpenGLView(frame: parent.frame)
+        parent.addSubview(view)
+        view.initializeAsSubview(mpvgl: mpv_gl)
+        return view
+    }
+
+    deinit {
+
+        // @todo: do we actually need this? Or do we use mpv_detach_destroy()?
+        if let mpv = mpv_handle {
+
+            mpv_terminate_destroy(mpv)
+        }
+    }
 }
