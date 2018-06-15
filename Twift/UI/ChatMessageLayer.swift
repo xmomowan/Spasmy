fe2a975cd510d0b1349d4db65cfe71b97c77889f
Ryan Liang <xmomowan@gmail.com>
2018-06-15 22:06:14 +0800

    Restructure folders

---
+++ Twift/UI/ChatMessageLayer.swift
@@ -0,0 +1,13 @@
+//
+//  ChatMessageLayer.swift
+//  Twift
+//
+//  Created by Ryan Liang on 6/20/18.
+//  Copyright © 2018 Ryan Liang. All rights reserved.
+//
+
+import Cocoa
+
+class ChatMessageLayer: CALayer {
+
+}
